4afd49dae0ec54e5ece139d2cae6a3a41cbdc6cc
AchodaJ <achoda67rc@gmail.com>
2022-09-08 03:59:14 -0700

    TASK 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
-							\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+		char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+			write(2, stri, 59);
+				return (1);
 }
